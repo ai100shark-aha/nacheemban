68ab40909d68c09848e67e67629c2d554eaf4603
상기 <ai100shark@gmail.com>
2026-05-29 07:58:15 +0900

    직업나침반 추가 + 통합 메뉴

--- compass/urls.py
+++ compass/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('career/', views.index, name='career'),
     path('api/db/', views.get_db, name='get_db'),
     path('api/db/update/', views.update_db, name='update_db'),
     path('api/db/merge/', views.merge_db, name='merge_db'),
